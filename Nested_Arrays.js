326cb02cfc998d0819bd1e8e83f1088b1c45734d
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-08-14 16:44:03 -0500

    Create Nested_Arrays.js

---
+++ Nested_Arrays.js
@@ -0,0 +1,12 @@
+// you can access elements using this technique
+// nested arrays
+let numberClusters = [[1,2], [3,4],[5,6]];
+
+// the first number specifies the element, the second number specifies the position in the element
+const target = numberClusters[2][1];
+console.log(target)
+
+const target2 = numberClusters[1][0];
+console.log(target2);
+
+console.log(numberClusters[0][1]);
