8650f5a4c6912d82a92a5b25c49b84bcdcf9d72a
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-10-13 21:59:42 -0500

    Create ITERATORS.js

---
+++ ITERATORS.js
@@ -0,0 +1,6 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+// this is how you use a foreach loop using arrow notation
+fruits.forEach(f =>
+console.log('I want to eat a ' + f));
