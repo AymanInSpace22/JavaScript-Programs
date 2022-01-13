929a7d2b9f94e2c8becbeabc984161c198c8603d
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2022-01-13 17:29:20 -0600

    Update p5.js_createCanvas.js

--- p5.js_createCanvas.js
+++ p5.js_createCanvas.js
@@ -2,12 +2,25 @@ function setup(){
   // Setup code goes here
   createCanvas(400, 400);
   // TODO: Set the background color of the canvas
-  //background(150);
+  //background(150); static canvas
   background(Math.floor(Math.random() * 256));
-
 }
 
-function draw(){
-  // Drawing code goes here
 
+
+
+//put bg color in draw function to get dynamic bg
+function setup() {
+  // Your setup code goes here
+  createCanvas(400, 400);
+  // Run the draw() function once per second (you will learn about the frameRate() function in a later lesson)
+  frameRate(1);
+  // TODO: Move the background() function below to the draw() function
 }
+
+function draw() {
+  // Your drawing code goes here
+  // TODO: Set background color to a random gray value
+  background(Math.floor(Math.random() * 256));
+}
+
