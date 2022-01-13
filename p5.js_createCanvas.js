e18bec06064c119ecbdc6a00db6cbbb4dcc20050
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2022-01-13 16:51:03 -0600

    Create p5.js_createCanvas.js

---
+++ p5.js_createCanvas.js
@@ -0,0 +1,13 @@
+function setup(){
+  // Setup code goes here
+  createCanvas(400, 400);
+  // TODO: Set the background color of the canvas
+  //background(150);
+  background(Math.floor(Math.random() * 256));
+
+}
+
+function draw(){
+  // Drawing code goes here
+
+}
