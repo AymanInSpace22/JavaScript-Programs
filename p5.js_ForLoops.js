0ccdd615acb8ba9ca32a9e8cf7d9b9595e54a265
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2022-01-14 21:11:34 -0600

    Create p5.js_ForLoops.js

---
+++ p5.js_ForLoops.js
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(400, 400);
+  background(200);
+}
+
+function draw() {
+  // TODO: Comment out the below rectangle functions
+  /*
+  rect(25, 25, 50, 50);
+  rect(100, 25, 50, 50);
+  rect(175, 25, 50, 50);
+  rect(250, 25, 50, 50);
+  rect(325, 25, 50, 50);
+  */
+
+  /* refactored
+  for(let i = 0; i < 5; i++){
+    rect(25 + i * 75, 25, 50, 50);
+  }
+  */
+
+  // TODO: Draw a grid of rectangles using for loops
+  // Refactored plus a nested loop
+  for(let posX = 0; posX < 5; posX++){
+    for(let posY = 0; posY < 5; posY++) {
+      rect(25+ posX * 75, 25 + posY * 75, 50, 50);
+    }
+  }
+}
+
+
+
+
+
