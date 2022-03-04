50db22e7881ef78d9b3cbccc60451da1e8871c47
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2022-03-03 20:56:43 -0600

    Update DOM_manipulation.js

--- DOM_manipulation.js
+++ DOM_manipulation.js
@@ -1,6 +1,5 @@
 // style property
 // this is 2 diff ways to alter the CSS with the DOM
-
 let bgColor = document.querySelector('body');
 bgColor.style.backgroundColor = '#201F2E';
 
@@ -9,7 +8,6 @@ document.querySelector('body').style.backgroundColor = '#201F2E'
 
 
 // innerHTML property allows you to alter the content of an element
-
 document.querySelector('h1').innerHTML = 'Most popular TV show searches in 2016';
 
 document.querySelector('#fourth').innerHTML = 'Fourth element';
@@ -26,3 +24,13 @@ newDestination.id = 'oaxaca';
 newDestination.innerHTML = ' Oaxaca, Mexico';
 // finally, appending it to the ul element with an Id of #more-destinations
 document.querySelector('#more-destinations').appendChild(newDestination);
+
+
+
+
+// removeChild method removes a child node from a specified parent node
+// this 1st way defines a variable, and passes that variable as an argument to the removeChild()
+let country = document.querySelector('#oaxaca');
+document.querySelector('#more-destinations').removeChild(country);
+// this 2nd was just selects the parent, then chains removeChild() and passes in the Id with another document.querySelector
+document.querySelector('#more-destinations').removeChild(document.querySelector('#oaxaca'));
