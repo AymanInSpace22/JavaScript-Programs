a98c73b83933465acd00ee99daea1a0b58771545
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2022-03-03 20:40:12 -0600

    Update DOM_manipulation.js

--- DOM_manipulation.js
+++ DOM_manipulation.js
@@ -13,3 +13,16 @@ document.querySelector('body').style.backgroundColor = '#201F2E'
 document.querySelector('h1').innerHTML = 'Most popular TV show searches in 2016';
 
 document.querySelector('#fourth').innerHTML = 'Fourth element';
+
+
+
+
+// appendChild method appends an element as the last child of the specified element
+// creating a list element
+let newDestination = document.createElement('li');
+// assigning it an Id
+newDestination.id = 'oaxaca';
+// assigning it contect with innerHTML
+newDestination.innerHTML = ' Oaxaca, Mexico';
+// finally, appending it to the ul element with an Id of #more-destinations
+document.querySelector('#more-destinations').appendChild(newDestination);
