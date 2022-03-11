5f76c08905de729abdda0c34b9a991a84e60fd5f
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2022-03-11 15:31:13 -0600

    Create Handlebars_each_this.js

---
+++ Handlebars_each_this.js
@@ -0,0 +1,38 @@
+
+const source = document.getElementById('eachHelper').innerHTML;
+const template = Handlebars.compile(source);
+
+const context = {
+  newArray: [1,2,3,4,5,6,7,8,9,10]
+};
+
+const compiledHtml = template(context);
+
+const displayElements = document.getElementById('display');
+displayElements.innerHTML = compiledHtml;
+
+
+
+
+//the HTML
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Each Block[s]</title>
+    <link href="public/style.css" type="text/css" rel="stylesheet">
+    <script src="handlebars.min.js"></script>
+    <!-- Add an {{each}} helper block in the <script> element below-->
+    <script id="eachHelper" type="text/x-handlebars-template">
+      {{#each newArray}}
+        <div class="block">{{this}}</div>
+      {{/each}}
+    </script>
+  </head>
+  <body>
+    <h1>Each Block[s]</h1>
+    <div id="display">
+    </div>
+    <script src="public/main.js" type="text/javascript"></script>
+  </body>
+</html>
