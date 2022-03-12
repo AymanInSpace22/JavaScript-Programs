51b9f74b9cf3d92ddc2855392428777f9e31d9dc
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2022-03-11 21:25:35 -0600

    Update Handlebars_each_this.js

--- Handlebars_each_this.js
+++ Handlebars_each_this.js
@@ -36,3 +36,58 @@ displayElements.innerHTML = compiledHtml;
     <script src="public/main.js" type="text/javascript"></script>
   </body>
 </html>
+        
+        
+        
+        
+        
+        
+        
+        
+// part 2
+const source = document.getElementById('languagesTemp').innerHTML;
+const template = Handlebars.compile(source);
+
+const context = {
+  languages: [
+    {name: 'HTML'},
+    {name: 'CSS'},
+    {name: 'JavaScript'}
+  ]
+};
+
+const compiledHtml = template(context);
+
+const displayGoals = document.getElementById('goals');
+displayGoals.innerHTML = compiledHtml;
+
+        
+        
+        
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Languages</title>
+    <link href="public/style.css" type="text/css" rel="stylesheet">
+    <script src="handlebars.min.js"></script>
+    <!-- Add an {{each}} helper block in the <script> element below-->
+    <script id="languagesTemp" type="text/x-handlebars-template">
+    {{#each languages}}
+      <div class="card">
+          <p>I should learn {{this.name}}.</p>
+      </div>
+    {{/each}}
+
+    </script>
+  </head>
+  <body>
+    <h1>
+      Thoughts on Languages:
+    </h1>
+    <div id="goals">
+    </div>
+  	<script src="public/main.js" type="text/javascript"></script>  
+  </body>
+</html>
+
